1ac057b6fc27881319d5febc26ae5dad2dd6918f
kurlo <kurcz.l3@gmail.com>
2020-06-10 01:18:16 +0200

    language corrected

--- src/components/menu.js
+++ src/components/menu.js
@@ -133,22 +133,22 @@ const Menu = ({main, color}) => {
         const url = typeof window !== "undefined" ? window.location.href : ""
         const slug = url.split("/").pop()
         if (intl.locale === "hu") {
-            
-            const direct = state.subLinks.find(x => x.slugs[1] === slug)
-            if (typeof direct === "undefined") {
-                return null
-            } else {
+            const direct = state.links.find(x => x.slugs[1] === slug)
+            if (typeof (direct) === "undefined") {
+                return state.subLinks.find(x => x.slugs[1] === slug).path_en
+            }
+            else {
                 return direct.path_en
             }
         } else {
-            const direct = state.subLinks.find(x => x.slugs[0] === slug)
-            if (typeof direct === "undefined") {
-                return null
-            } else {
+            const direct = state.links.find(x => x.slugs[0] === slug)
+            if (typeof (direct) === "undefined") {
+                return state.subLinks.find(x => x.slugs[0] === slug).path_hu
+            }
+            else {
                 return direct.path_hu
             }
         }
-        
     }
 
     return (
