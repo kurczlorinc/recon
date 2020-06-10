e2f858e6dd6e965ffd329f6a87e98e960717c6bb
kurlo <kurcz.l3@gmail.com>
2020-06-10 02:10:41 +0200

    changed first to null

--- src/components/menu.js
+++ src/components/menu.js
@@ -135,18 +135,18 @@ const Menu = ({main, color}) => {
         if (intl.locale === "hu") {
             const direct = state.links.find(x => x.slugs[1] === slug)
             if (typeof(direct) === "undefined") {
-                return state.subLinks.find(x => x.slugs[1] === slug).path_en
+                return null
             }
             else {
-                return null
+                return state.links.find(x => x.slugs[1] === slug).path_en
             }
         } else {
             const direct = state.links.find(x => x.slugs[0] === slug)
             if (typeof(direct) === "undefined") {
-                return state.subLinks.find(x => x.slugs[0] === slug).path_hu
+                return null
             }
             else {
-                return null
+                return state.links.find(x => x.slugs[0] === slug).path_hu
             }
         }
     }
