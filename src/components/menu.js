45d8ba71860b42ad189bd5bfd29c4f0a854a3688
kurlo <kurcz.l3@gmail.com>
2020-06-10 02:00:18 +0200

    copied line

--- src/components/menu.js
+++ src/components/menu.js
@@ -134,19 +134,19 @@ const Menu = ({main, color}) => {
         const slug = url.split("/").pop()
         if (intl.locale === "hu") {
             const direct = state.links.find(x => x.slugs[1] === slug)
-            if (typeof (direct) === "undefined") {
+            if (typeof(direct) === "undefined") {
                 return state.subLinks.find(x => x.slugs[1] === slug).path_en
             }
             else {
-                return direct.path_en
+                return state.links.find(x => x.slugs[1] === slug).path_en
             }
         } else {
             const direct = state.links.find(x => x.slugs[0] === slug)
-            if (typeof (direct) === "undefined") {
+            if (typeof(direct) === "undefined") {
                 return state.subLinks.find(x => x.slugs[0] === slug).path_hu
             }
             else {
-                return direct.path_hu
+                return state.links.find(x => x.slugs[0] === slug).path_hu
             }
         }
     }
