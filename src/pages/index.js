82ec00615a23dcc025083728167fafc2e4d107d8
kurlo <kurcz.l3@gmail.com>
2020-12-25 12:04:24 +0100

    alap-fotó back

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import favicon16 from "../images/fav_16x16.ico"
 import videoPoster from "../images/recon-start.jpg"
 import speakeroff from "../images/speaker-off.png"
 import speakeron from "../images/speaker-on.png"
+import og from "../images/recon-alap-foto-2.jpg"
 import Player from "@vimeo/player"
 import { useIntl } from "gatsby-plugin-intl"
 
@@ -81,7 +82,7 @@ const IndexPage = () => {
             <link rel="icon" type="image/png" href={favicon96} sizes="96x96" />
             <link rel="icon" type="image/png" href={favicon32} sizes="32x32" />
             <link rel="icon" type="image/png" href={favicon16} sizes="16x16" />
-            
+            <meta property="og:image" content={og} />
             <meta property="og:url" content={site.siteMetadata.siteUrl} />
             <script>
             vid=document.getElementById("background-video")
