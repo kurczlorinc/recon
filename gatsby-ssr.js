3ae78ab5de78149770a004f6ab98f9796d3bd92d
kurlo <kurcz.l3@gmail.com>
2020-12-25 13:16:43 +0100

    sssr change again

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,34 +1,38 @@
 const React = require("react")
 const { Helmet } = require("react-helmet")
 
-exports.onRenderBody = (
-  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
-  pluginOptions
-) => {
+export const onRenderBody = ({
+  setHeadComponents,
+  setHtmlAttributes,
+  setBodyAttributes,
+}) => {
   const helmet = Helmet.renderStatic()
   setHtmlAttributes(helmet.htmlAttributes.toComponent())
   setBodyAttributes(helmet.bodyAttributes.toComponent())
   setHeadComponents([
     helmet.title.toComponent(),
-    helmet.link.toComponent(),
+    helmet.base.toComponent(),
     helmet.meta.toComponent(),
+    helmet.link.toComponent(),
     helmet.noscript.toComponent(),
     helmet.script.toComponent(),
     helmet.style.toComponent(),
   ])
 }
 
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
   const headComponents = getHeadComponents()
+  const order = ["title", "base", "meta", "link", "noscript", "script", "style"]
 
-  headComponents.sort((x, y) => {
-    if (x.props && x.props["data-react-helmet"]) {
-      return -1
-    } else if (y.props && y.props["data-react-helmet"]) {
-      return 1
-    }
-    return 0
-  })
+  const sortedHeadComponents = headComponents
+    .slice(0)
+    .flat()
+    .sort((x, y) => {
+      return order.indexOf(x.type) - order.indexOf(y.type)
+    })
 
-  replaceHeadComponents(headComponents)
+  replaceHeadComponents(sortedHeadComponents)
 }
